32417f8f653c1e958495edcd5f5f7b0cecef1787
Utk1rs8 <utkarshchurariya19@gmail.com>
2024-09-24 14:52:56 +0530

    footer

--- internshipapp/src/Layout.js
+++ internshipapp/src/Layout.js
@@ -1,7 +1,9 @@
 import { Link, Outlet } from "react-router-dom";
 import { FaShoppingCart } from "react-icons/fa";
-
-
+import { FaInstagram } from "react-icons/fa";
+import { FaSnapchatSquare } from "react-icons/fa";
+import { FaWhatsappSquare } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
 
 const Layout=()=>{
     return(
@@ -34,6 +36,63 @@ const Layout=()=>{
         </div>
 
         <Outlet/>
+
+        <div class="mlk">
+            <div class="ko">
+                <div className="footer-box">
+                    <h1 className="fo-wri">Category</h1>
+                    <h3 className="fo-para">Running Shoes</h3>
+                    <h3 className="fo-para">Yoga Shoes</h3>
+                    <h3 className="fo-para">Sports Shoes</h3>
+                    <h3 className="fo-para">Casual Shoes</h3>
+                </div>
+                <div className="footer-box">
+                    <h1 className="fo-wri">Business</h1>
+                    <h3 className="fo-para">Company Profile</h3>    
+                    <h3 className="fo-para">Social Responsibility</h3>
+                    <h3 className="fo-para">Brand Kit</h3>
+                    <h3 className="fo-para">Media</h3>
+                    <h3 className="fo-para">Employee</h3>
+                    <h3 className="fo-para">Privacy</h3>
+                    <h3 className="fo-para">Terms and Conditions</h3>
+                </div>
+                <div className="footer-box">
+                    <h1 className="fo-wri">Useful Links</h1>
+                    <h3 className="fo-para">Return Policies</h3>
+                    <h3 className="fo-para">Announcement</h3>
+                    <h3 className="fo-para">Have Issue?</h3>
+                    <h3 className="fo-para">Media Kit</h3>
+                </div>
+                <div className="footer-box">
+                    <h1 className="fo-wri">Support Center</h1>
+                    <h3 className="fo-para">utkarshchurariya19@gmail.com</h3>
+                    <h3 className="fo-para">8982260970</h3>
+                    <h3 className="fo-para">Ride It,Kolar Road,Bhopal,M.P</h3>
+                </div>
+                <div>
+                    <h1 className="fo-wri">Follow Us</h1>
+                    <div className="f-icon">
+                        <FaInstagram />
+                        <FaSnapchatSquare />
+                        <br/>
+                        <FaWhatsappSquare />
+                        <FaSquareXTwitter />
+                    </div>
+                </div>
+        </div>
+        <br/>
+        <br/>
+        <br/>
+        <div class="kkk">
+            <div id="logo">
+                <h1>Ride</h1>
+            </div>            
+            <div>
+                <h1 className="fo-wri">@2024 RIDE IT,All Rights Reserved.</h1>
+            </div>
+        </div>
+        </div>
+
         </>
     )
 }
