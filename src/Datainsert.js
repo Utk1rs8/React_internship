40dacbb0b7de7f4c1163190434946319b4ab7df2
Utk1rs8 <utkarshchurariya19@gmail.com>
2024-10-07 12:39:39 +0530

    datainsert

--- src/Datainsert.js
+++ src/Datainsert.js
@@ -1,128 +1,91 @@
 import React, { useState } from 'react';
 import axios from "axios";
-const Datainsert= () => {
-    const [input, setInput]=useState({});
-    const handleInput=(e)=>
-    {
-        let name=e.target.name;
-        let value=e.target.value;
-        setInput(values=>({...values, [name]:value}));
-    }
-    const insertData=()=>
-    {
-        let url="http://127.0.0.1:8000/";
+
+const Datainsert = () => {
+    const [input, setInput] = useState({});
+    const [imageFile, setImageFile] = useState(null); // New state for file
+
+    const insertData = () => {
+        let url = "http://127.0.0.1:8000/";
         console.log(input);
-        axios.post(url,{name:input.name,price:input.price,image:input.image,engine_capacity:input.engine_capacity,mileage:input.mileage,transmission:input.transmission,weight:input.weight,fuel_tank:input.fuel_tank,power:input.power,tourque:input.tourque})
-        .then((res)=>{
-            alert("data inserted");
-        })
+
+        // Create FormData object for handling files
+        const formData = new FormData();
+        formData.append('name', input.name);
+        formData.append('price', input.price);
+        formData.append('image', imageFile); // Image file handled separately
+        formData.append('engine_capacity', input.engine_capacity);
+        formData.append('mileage', input.mileage);
+        formData.append('transmission', input.transmission);
+        formData.append('weight', input.weight);
+        formData.append('fuel_tank', input.fuel_tank);
+        formData.append('power', input.power);
+        formData.append('tourque', input.tourque); // Changed back to 'tourque'
+
+        axios.post(url, formData)
+            .then((res) => {
+                alert("Data inserted");
+            })
+            .catch((error) => {
+                console.error("There was an error inserting data:", error);
+            });
     }
 
-  return (
-    <>
-      <div>
-        <label>Name:</label>
-        <input
-          type="text"
-          name="name"
-          value={input.name}
-          onChange={handleInput}
-          required
-        />
-      </div>
-      <div>
-        <label>Price:</label>
-        <input
-          type="number"
-          name="price"
-          value={input.price}
-          onChange={handleInput}
-          required
-        />
-      </div>
-      <div>
-        <label>Image:</label>
-        <input
-          type="file"
-          name="image"
-          value={input.image}
-          onChange={handleInput}
-          required
-        />
-      </div>
-      <div>
-        <label>Engine Capacity:</label>
-        <input
-          type="text"
-          name="engine_capacity"
-          value={input.engine_capacity}
-          onChange={handleInput}
-          required
-        />
-      </div>
-      <div>
-        <label>Mileage:</label>
-        <input
-          type="text"
-          name="mileage"
-          value={input.mileage}
-          onChange={handleInput}
-          required
-        />
-      </div>
-      <div>
-        <label>Transmission:</label>
-        <input
-          type="text"
-          name="transmission"
-          value={input.transmission}
-          onChange={handleInput}
-          required
-        />
-      </div>
-      <div>
-        <label>Weight:</label>
-        <input
-          type="text"
-          name="weight"
-          value={input.weight}
-          onChange={handleInput}
-          required
-        />
-      </div>
-      <div>
-        <label>Fuel Tank:</label>
-        <input
-          type="text"
-          name="fuel_tank"
-          value={input.fuel_tank}
-          onChange={handleInput}
-          required
-        />
-      </div>
-      <div>
-        <label>Power:</label>
-        <input
-          type="text"
-          name="power"
-          value={input.power}
-          onChange={handleInput}
-          required
-        />
-      </div>
-      <div>
-        <label>Torque:</label>
-        <input
-          type="text"
-          name="tourque"
-          value={input.tourque}
-          onChange={handleInput}
-          required
-        />
-      </div>
-      <button onClick={insertData}>Insert</button>
-      </>
-  );
+    const handleInput = (e) => {
+        const name = e.target.name;
+        const value = e.target.value;
+        setInput(values => ({ ...values, [name]: value }));
+    }
+
+    const handleFileInput = (e) => {
+        setImageFile(e.target.files[0]); // Update the image file state
+    }
+
+    return (
+        <>
+            <div>
+                <label>Name:</label>
+                <input type="text" name="name" value={input.name || ''} onChange={handleInput} />
+            </div>
+            <div>
+                <label>Price:</label>
+                <input type="number" name="price" value={input.price || ''} onChange={handleInput} />
+            </div>
+            <div>
+                <label>Image:</label>
+                <input type="file" name="image" onChange={handleFileInput} /> {/* File input */}
+            </div>
+            <div>
+                <label>Engine Capacity:</label>
+                <input type="text" name="engine_capacity" value={input.engine_capacity || ''} onChange={handleInput} />
+            </div>
+            <div>
+                <label>Mileage:</label>
+                <input type="text" name="mileage" value={input.mileage || ''} onChange={handleInput} />
+            </div>
+            <div>
+                <label>Transmission:</label>
+                <input type="text" name="transmission" value={input.transmission || ''} onChange={handleInput} />
+            </div>
+            <div>
+                <label>Weight:</label>
+                <input type="text" name="weight" value={input.weight || ''} onChange={handleInput} />
+            </div>
+            <div>
+                <label>Fuel Tank:</label>
+                <input type="text" name="fuel_tank" value={input.fuel_tank || ''} onChange={handleInput} />
+            </div>
+            <div>
+                <label>Power:</label>
+                <input type="text" name="power" value={input.power || ''} onChange={handleInput} />
+            </div>
+            <div>
+                <label>Tourque:</label> {/* Changed label back to 'Tourque' */}
+                <input type="text" name="tourque" value={input.tourque || ''} onChange={handleInput} />
+            </div>
+            <button onClick={insertData}>Insert</button>
+        </>
+    );
 };
 
 export default Datainsert;
