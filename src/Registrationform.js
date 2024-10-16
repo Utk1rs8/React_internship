8acf22715153d4fe44ec24b278a652284770bf08
Utk1rs8 <utkarshchurariya19@gmail.com>
2024-10-16 13:51:33 +0530

    registration form

--- src/Registrationform.js
+++ src/Registrationform.js
@@ -3,37 +3,36 @@ import React from 'react'
 import { FaUserAlt } from "react-icons/fa";
 import { FaLock } from "react-icons/fa";
 import { Link,Outlet } from 'react-router-dom';
-
+import { FaPhoneAlt } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
 const Registrationform=()=>{
     return(
         <>
    <div id="Loginform">
    <div className='wrapper'>
         <form action="">
-            <h1>Login</h1>
-            <div>
-            <label>Bike Type:</label>
-            <select name="client_admin" id="client_admin">
-                <option value="client">Client</option>
-                <option value="admin">Admin</option>
-            </select>
-            </div>
+            <h1>Registration</h1>
             <div className="input-box">
                 <input type="text" placeholder="Username" required/>
                 <FaUserAlt className='icon'/>
             </div>
             <div className="input-box">
+                <input type="email" placeholder="Email" required/>
+                <MdEmail className='icon'/>
+            </div>
+            <div className='input-box'>
+                <input type='tel' placeholder='Contact' required/>
+                <FaPhoneAlt className='icon'/>
+            </div>
+            <div className="input-box">
                 <input type="password" placeholder="Password" required/>
                 <FaLock className='icon'/>
             </div>
-            <div className="remember-forgot">
-                <label><input type="checkbox" />Remember me</label>
-                <a href="#">Forgot password?</a>
+            <div className="input-box">
+                <input type="password" placeholder="Confirm Password" required/>
+                <FaLock className='icon'/>
             </div>
-            <button type='submit'><Link id='login-button' to="../Adminlogin">Login</Link></button>
-            <div className="register-link">
-                <p>Don't have an account?<Link to="../registrationform">Register</Link></p>
-            </div>
+            <button type='submit'><Link id='login-button' to="">Registration</Link></button>
         </form>
     </div>
 
