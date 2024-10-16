5eb3f0b7a44b213b27895bad7546d22935df94ed
Utk1rs8 <utkarshchurariya19@gmail.com>
2024-10-16 13:39:02 +0530

    registration page

--- src/Registrationform.js
+++ src/Registrationform.js
@@ -1,9 +1,46 @@
+import React from 'react'
+// import './LoginForm.css'
+import { FaUserAlt } from "react-icons/fa";
+import { FaLock } from "react-icons/fa";
+import { Link,Outlet } from 'react-router-dom';
+
 const Registrationform=()=>{
     return(
         <>
-        <h1>Registration page</h1>
-        
-        </>
+   <div id="Loginform">
+   <div className='wrapper'>
+        <form action="">
+            <h1>Login</h1>
+            <div>
+            <label>Bike Type:</label>
+            <select name="client_admin" id="client_admin">
+                <option value="client">Client</option>
+                <option value="admin">Admin</option>
+            </select>
+            </div>
+            <div className="input-box">
+                <input type="text" placeholder="Username" required/>
+                <FaUserAlt className='icon'/>
+            </div>
+            <div className="input-box">
+                <input type="password" placeholder="Password" required/>
+                <FaLock className='icon'/>
+            </div>
+            <div className="remember-forgot">
+                <label><input type="checkbox" />Remember me</label>
+                <a href="#">Forgot password?</a>
+            </div>
+            <button type='submit'><Link id='login-button' to="../Adminlogin">Login</Link></button>
+            <div className="register-link">
+                <p>Don't have an account?<Link to="../registrationform">Register</Link></p>
+            </div>
+        </form>
+    </div>
+
+   </div>
+   <Outlet/>
+   
+   </>
     )
 }
 
