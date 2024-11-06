8d805069b36cf9e81e127c1970c0a6deaf5cb80b
Utk1rs8 <utkarshchurariya19@gmail.com>
2024-11-06 14:30:48 +0530

    subscription page

--- src/SubscriptionPage.js
+++ src/SubscriptionPage.js
@@ -1,11 +1,250 @@
-const SubscriptionPage=()=>{
-    return(
+const SubscriptionPage = () => {
+    return (
         <>
-        <h1>SubscriptionPage</h1>
-        
-        
+            <div id="subscription-premium">
+                <div className="subscriptionCard">
+                    <div className="subscriptionCard__border"></div>
+                    <div className="subscriptionCard_titleContainer">
+                        <span className="subscriptionCard_title">Free</span>
+                        <p className="subscriptionCard_paragraph">No advantages</p>
+                        <h3 style={{color: "white"}}>₹0</h3>
+                    </div>
+                    <hr className="subscriptionLine" />
+                    <ul className="subscriptionCard__list">
+                        <li className="subscriptionCard__listItem">
+                            <span className="subscriptionCheck">
+                                <svg
+                                    className="subscriptionCheck_svg"
+                                    fill="currentColor"
+                                    viewBox="0 0 16 16"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                >
+                                    <path
+                                        clipRule="evenodd"
+                                        d="M12.416 3.376a.75.75 0 0 1 .208 1.04l-5 7.5a.75.75 0 0 1-1.154.114l-3-3a.75.75 0 0 1 1.06-1.06l2.353 2.353 4.493-6.74a.75.75 0 0 1 1.04-.207Z"
+                                        fillRule="evenodd"
+                                    ></path>
+                                </svg>
+                            </span>
+                            <span className="subscriptionList_text">No Free Services</span>
+                        </li>
+                        <li className="subscriptionCard__listItem">
+                            <span className="subscriptionCheck">
+                                <svg
+                                    className="subscriptionCheck_svg"
+                                    fill="currentColor"
+                                    viewBox="0 0 16 16"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                >
+                                    <path
+                                        clipRule="evenodd"
+                                        d="M12.416 3.376a.75.75 0 0 1 .208 1.04l-5 7.5a.75.75 0 0 1-1.154.114l-3-3a.75.75 0 0 1 1.06-1.06l2.353 2.353 4.493-6.74a.75.75 0 0 1 1.04-.207Z"
+                                        fillRule="evenodd"
+                                    ></path>
+                                </svg>
+                            </span>
+                            <span className="subscriptionList_text">No Maintainance</span>
+                        </li>
+                        <li className="subscriptionCard__listItem">
+                            <span className="subscriptionCheck">
+                                <svg
+                                    className="subscriptionCheck_svg"
+                                    fill="currentColor"
+                                    viewBox="0 0 16 16"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                >
+                                    <path
+                                        clipRule="evenodd"
+                                        d="M12.416 3.376a.75.75 0 0 1 .208 1.04l-5 7.5a.75.75 0 0 1-1.154.114l-3-3a.75.75 0 0 1 1.06-1.06l2.353 2.353 4.493-6.74a.75.75 0 0 1 1.04-.207Z"
+                                        fillRule="evenodd"
+                                    ></path>
+                                </svg>
+                            </span>
+                            <span className="subscriptionList_text">No Spare Parts</span>
+                        </li>
+                        <li className="subscriptionCard__listItem">
+                            <span className="subscriptionCheck">
+                                <svg
+                                    className="subscriptionCheck_svg"
+                                    fill="currentColor"
+                                    viewBox="0 0 16 16"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                >
+                                    <path
+                                        clipRule="evenodd"
+                                        d="M12.416 3.376a.75.75 0 0 1 .208 1.04l-5 7.5a.75.75 0 0 1-1.154.114l-3-3a.75.75 0 0 1 1.06-1.06l2.353 2.353 4.493-6.74a.75.75 0 0 1 1.04-.207Z"
+                                        fillRule="evenodd"
+                                    ></path>
+                                </svg>
+                            </span>
+                            <span className="subscriptionList_text">No Extra Benifits</span>
+                        </li>
+                    </ul>
+                    <button className="subscriptionButton">Get Subscription</button>
+                </div>
+                <div className="subscriptionCard">
+                    <div className="subscriptionCard__border"></div>
+                    <div className="subscriptionCard_titleContainer">
+                        <span className="subscriptionCard_title">Standard</span>
+                        <p className="subscriptionCard_paragraph">Some advantages</p>
+                        <h3 style={{color: "white"}}>₹24999</h3>
+                    </div>
+                    <hr className="subscriptionLine" />
+                    <ul className="subscriptionCard__list">
+                        <li className="subscriptionCard__listItem">
+                            <span className="subscriptionCheck">
+                                <svg
+                                    className="subscriptionCheck_svg"
+                                    fill="currentColor"
+                                    viewBox="0 0 16 16"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                >
+                                    <path
+                                        clipRule="evenodd"
+                                        d="M12.416 3.376a.75.75 0 0 1 .208 1.04l-5 7.5a.75.75 0 0 1-1.154.114l-3-3a.75.75 0 0 1 1.06-1.06l2.353 2.353 4.493-6.74a.75.75 0 0 1 1.04-.207Z"
+                                        fillRule="evenodd"
+                                    ></path>
+                                </svg>
+                            </span>
+                            <span className="subscriptionList_text">2 Free Services</span>
+                        </li>
+                        <li className="subscriptionCard__listItem">
+                            <span className="subscriptionCheck">
+                                <svg
+                                    className="subscriptionCheck_svg"
+                                    fill="currentColor"
+                                    viewBox="0 0 16 16"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                >
+                                    <path
+                                        clipRule="evenodd"
+                                        d="M12.416 3.376a.75.75 0 0 1 .208 1.04l-5 7.5a.75.75 0 0 1-1.154.114l-3-3a.75.75 0 0 1 1.06-1.06l2.353 2.353 4.493-6.74a.75.75 0 0 1 1.04-.207Z"
+                                        fillRule="evenodd"
+                                    ></path>
+                                </svg>
+                            </span>
+                            <span className="subscriptionList_text">1 year Maintainance (2 times)</span>
+                        </li>
+                        <li className="subscriptionCard__listItem">
+                            <span className="subscriptionCheck">
+                                <svg
+                                    className="subscriptionCheck_svg"
+                                    fill="currentColor"
+                                    viewBox="0 0 16 16"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                >
+                                    <path
+                                        clipRule="evenodd"
+                                        d="M12.416 3.376a.75.75 0 0 1 .208 1.04l-5 7.5a.75.75 0 0 1-1.154.114l-3-3a.75.75 0 0 1 1.06-1.06l2.353 2.353 4.493-6.74a.75.75 0 0 1 1.04-.207Z"
+                                        fillRule="evenodd"
+                                    ></path>
+                                </svg>
+                            </span>
+                            <span className="subscriptionList_text">3 Spare Parts Repair & Change</span>
+                        </li>
+                        <li className="subscriptionCard__listItem">
+                            <span className="subscriptionCheck">
+                                <svg
+                                    className="subscriptionCheck_svg"
+                                    fill="currentColor"
+                                    viewBox="0 0 16 16"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                >
+                                    <path
+                                        clipRule="evenodd"
+                                        d="M12.416 3.376a.75.75 0 0 1 .208 1.04l-5 7.5a.75.75 0 0 1-1.154.114l-3-3a.75.75 0 0 1 1.06-1.06l2.353 2.353 4.493-6.74a.75.75 0 0 1 1.04-.207Z"
+                                        fillRule="evenodd"
+                                    ></path>
+                                </svg>
+                            </span>
+                            <span className="subscriptionList_text">Less Import Charges</span>
+                        </li>
+                    </ul>
+                    <button className="subscriptionButton">Get Subscription</button>
+                </div>
+                <div className="subscriptionCard">
+                    <div className="subscriptionCard__border"></div>
+                    <div className="subscriptionCard_titleContainer">
+                        <span className="subscriptionCard_title">Premium</span>
+                        <p className="subscriptionCard_paragraph">Many Advantages</p>
+                        <h3 style={{color: "white"}}>₹54999</h3>
+                    </div>
+                    <hr className="subscriptionLine" />
+                    <ul className="subscriptionCard__list">
+                        <li className="subscriptionCard__listItem">
+                            <span className="subscriptionCheck">
+                                <svg
+                                    className="subscriptionCheck_svg"
+                                    fill="currentColor"
+                                    viewBox="0 0 16 16"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                >
+                                    <path
+                                        clipRule="evenodd"
+                                        d="M12.416 3.376a.75.75 0 0 1 .208 1.04l-5 7.5a.75.75 0 0 1-1.154.114l-3-3a.75.75 0 0 1 1.06-1.06l2.353 2.353 4.493-6.74a.75.75 0 0 1 1.04-.207Z"
+                                        fillRule="evenodd"
+                                    ></path>
+                                </svg>
+                            </span>
+                            <span className="subscriptionList_text">3 Free Services</span>
+                        </li>
+                        <li className="subscriptionCard__listItem">
+                            <span className="subscriptionCheck">
+                                <svg
+                                    className="subscriptionCheck_svg"
+                                    fill="currentColor"
+                                    viewBox="0 0 16 16"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                >
+                                    <path
+                                        clipRule="evenodd"
+                                        d="M12.416 3.376a.75.75 0 0 1 .208 1.04l-5 7.5a.75.75 0 0 1-1.154.114l-3-3a.75.75 0 0 1 1.06-1.06l2.353 2.353 4.493-6.74a.75.75 0 0 1 1.04-.207Z"
+                                        fillRule="evenodd"
+                                    ></path>
+                                </svg>
+                            </span>
+                            <span className="subscriptionList_text">2 Year Maintainance</span>
+                        </li>
+                        <li className="subscriptionCard__listItem">
+                            <span className="subscriptionCheck">
+                                <svg
+                                    className="subscriptionCheck_svg"
+                                    fill="currentColor"
+                                    viewBox="0 0 16 16"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                >
+                                    <path
+                                        clipRule="evenodd"
+                                        d="M12.416 3.376a.75.75 0 0 1 .208 1.04l-5 7.5a.75.75 0 0 1-1.154.114l-3-3a.75.75 0 0 1 1.06-1.06l2.353 2.353 4.493-6.74a.75.75 0 0 1 1.04-.207Z"
+                                        fillRule="evenodd"
+                                    ></path>
+                                </svg>
+                            </span>
+                            <span className="subscriptionList_text">5 Spare Parts Repair & Change</span>
+                        </li>
+                        <li className="subscriptionCard__listItem">
+                            <span className="subscriptionCheck">
+                                <svg
+                                    className="subscriptionCheck_svg"
+                                    fill="currentColor"
+                                    viewBox="0 0 16 16"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                >
+                                    <path
+                                        clipRule="evenodd"
+                                        d="M12.416 3.376a.75.75 0 0 1 .208 1.04l-5 7.5a.75.75 0 0 1-1.154.114l-3-3a.75.75 0 0 1 1.06-1.06l2.353 2.353 4.493-6.74a.75.75 0 0 1 1.04-.207Z"
+                                        fillRule="evenodd"
+                                    ></path>
+                                </svg>
+                            </span>
+                            <span className="subscriptionList_text">Less Import Charges & Free Bike Dilevery</span>
+                        </li>
+                    </ul>
+                    <button className="subscriptionButton">Get Subscription</button>
+                </div>
+            </div>
         </>
-    )
-}
+    );
+};
 
 export default SubscriptionPage;
