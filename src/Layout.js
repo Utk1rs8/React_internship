e25aa902ad0d4131af7bccee6a7cc17b55f08f66
@nishantmalviya-90 <nishantmalviya69@gmail.com>
2024-11-09 23:30:34 +0530

    Update CSS

--- src/Layout.js
+++ src/Layout.js
@@ -35,33 +35,10 @@ const Layout = () => {
                         <div id="logreg">
                             {user ? (
                                 <>
-                                    <span>Welcome, {user.email}</span>
-                                    <button onClick={handleLogout} className="c-button c-button--gooey">
-                                        Logout
-                                        <div className="c-button__blobs">
-                                            <div></div>
-                                            <div></div>
-                                            <div></div>
-                                        </div>
-                                    </button>
-                                    <svg
-                                        style={{ display: 'block', height: 0, width: 0 }}
-                                        version="1.1"
-                                        xmlns="http://www.w3.org/2000/svg"
-                                    >
-                                        <defs>
-                                            <filter id="goo">
-                                                <feGaussianBlur result="blur" stdDeviation="10" in="SourceGraphic" />
-                                                <feColorMatrix
-                                                    result="goo"
-                                                    values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -7"
-                                                    mode="matrix"
-                                                    in="blur"
-                                                />
-                                                <feBlend in2="goo" in="SourceGraphic" />
-                                            </filter>
-                                        </defs>
-                                    </svg>
+                                    <span>Welcome, {user.email} </span>
+                                    <button id='Button-Logout' onClick={handleLogout} > Logout</button>
+
+                                    
                                 </>
                             ) : (
                                 <>
